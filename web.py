38dce2255d7334f40cfcea7998fc9486d3a0548c
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-29 15:48:32 +0900

    Update web.py
    help주석처리

--- web.py
+++ web.py
@@ -317,9 +317,8 @@ with empty_t2:
 
 radio_sel1_3 = st.multiselect(f"수집 뉴스 조회", ["상장 연기", "상장 철회", "프리 IPO", "기술특례상장", "상장예비심사", "IPO 주관", "IPO 수요예측"]
                               , default=["상장 연기", "상장 철회", "프리 IPO", "기술특례상장", "상장예비심사", "IPO 주관", "IPO 수요예측"],
-                              key='part2_1', max_selections=7
-                              ,
-                              help=f"전체 기사 수 : {len_2_1}   →   중복기업기사 제거 경우의 기사수 : {true_type_1.shape[0]}이며, 각 열별로 원하시는 정렬이 가능합니다.")
+                              key='part2_1', max_selections=7)
+                              # help=f"전체 기사 수 : {len_2_1}   →   중복기업기사 제거 경우의 기사수 : {true_type_1.shape[0]}이며, 각 열별로 원하시는 정렬이 가능합니다.")
 
 multi_select_1 = st.multiselect(f"기업 선택", list(company_2day.제목내최초기업명), default=list(company_2day.제목내최초기업명), key="test_about_word", max_selections=len(list(company_2day.제목내최초기업명)))
 
@@ -451,8 +450,8 @@ radio_sel2_3 = st.multiselect(f"수집 뉴스 조회",
                                "LP Limited Partner", "컨소시엄", "볼트온", "롤업", "롤링베이시스", "LOI", "투자유치"]
                               , default=["사모펀드", "Private Equity", "경영권 인수", "공개매수", "M&A", "블록딜", "지배구조개선", "소수지분",
                                          "일감몰아주기", "LP Limited Partner", "컨소시엄", "볼트온", "롤업", "롤링베이시스", "LOI", "투자유치"],
-                              key='part2_2', max_selections=len(pe_search_list),
-                              help=f"검색어 선택 전체 기사 수 : {len_2_2}   →   중복기업기사 제거경우의 기사수 : {true_type_2.shape[0]}이며, 각 열별로 원하시는 정렬이 가능합니다.")
+                              key='part2_2', max_selections=len(pe_search_list))
+                              # help=f"검색어 선택 전체 기사 수 : {len_2_2}   →   중복기업기사 제거경우의 기사수 : {true_type_2.shape[0]}이며, 각 열별로 원하시는 정렬이 가능합니다.")
 
 multi_select_2 = st.multiselect(f"기업 선택", list(company_2day_PE.제목내최초기업명), default=list(company_2day_PE.제목내최초기업명), key="test_about_word2", max_selections=len(list(company_2day_PE.제목내최초기업명)))
 
@@ -592,8 +591,8 @@ with empty_t6:
 
 radio_sel3_3 = st.multiselect(f"수집 뉴스 조회", ["회사채", "회사채 수요예측", "회사채 발행", "회사채 공모", "유상증자", "메자닌", "영구채"]
                               , default=["회사채", "회사채 수요예측", "회사채 발행", "회사채 공모", "유상증자", "메자닌", "영구채"], key='part2_3',
-                              max_selections=len(company_search_list),
-                              help=f"전체 기사 수 : {len_2_3}   →   중복기업기사 제거경우의 기사수 : {true_type_3.shape[0]}이며, 각 열별로 원하시는 정렬이 가능합니다.")
+                              max_selections=len(company_search_list))
+                              # help=f"전체 기사 수 : {len_2_3}   →   중복기업기사 제거경우의 기사수 : {true_type_3.shape[0]}이며, 각 열별로 원하시는 정렬이 가능합니다.")
 
 multi_select_3 = st.multiselect(f"기업 선택", list(company_2day_com.제목내최초기업명), default=list(company_2day_com.제목내최초기업명), key="test_about_word3", max_selections=len(list(company_2day_com.제목내최초기업명)))
 if skipped_button_company:
@@ -605,3 +604,5 @@ else:
     st.dataframe(COM_df[COM_df['검색어'].isin(radio_sel3_3)& (COM_df['제목내최초기업명'].isin(multi_select_3))].reset_index(drop=True), 200000, 500, use_container_width=True)
 # st.markdown(df[df['검색어'].isin(radio_sel3_3)].reset_index(drop=True).to_html(render_links=True), unsafe_allow_html=True)
 '-----'
+
+# st.dataframe(type1)
